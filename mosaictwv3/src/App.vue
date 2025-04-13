7b38afe903d070ffb205014b664f3f1e41fcf447
Eutatsu <eudald.inventor@gmail.com>
2025-04-13 02:31:14 +0200

    trasllat

--- mosaictwv3/src/App.vue
+++ mosaictwv3/src/App.vue
@@ -1,67 +1,13 @@
 <template>
-  <div id="app" class="flex flex-col w-full">
-    <nav class="bg-red-600 w-full drop-shadow fixed">
-      <div class="flex md:flex-row flex-col  md:items-center justify-between p-2 mx-auto max-w-screen-xl">
-      <div class="flex items-center justify-between"><router-link to="/" alt="Pagina d'inici"><div class="flex items-center hover:bg-red-500 rounded-sm">
-      <img src="./assets/Mosaic-Logo-SF.png" class="md:w-16 w-12" >
-      <h1 class="text-white font-bold text-xl m-3 mr-5 text-nowrap">Mosaic Casteller</h1>
-      
-      </div></router-link>
-      <button @click="ObrirNav()" class="md:hidden text-3xl text-white justify-end px-4 py-2 hover:bg-red-500">
-        <font-awesome-icon :icon="['fas', 'bars']" /> 
-      </button></div>
-      <div  :class="open?'inline':'hidden'" class="md:inline transition-all">
-        <ul class="flex md:flex-row flex-col md:items-center text-base gap-1 font-bold text-white mt-3 md:mt-0">
-       <router-link @click="ObrirNav()" to="/" ><li class="hover:bg-red-500 py-2 px-3 text-nowrap rounded-sm"><button >Mosaic</button></li></router-link>
-       <router-link @click="ObrirNav()" to="/liniatemporal"> <li class="hover:bg-red-500 py-2 px-3 text-nowrap rounded-sm"><button >Linia Temporal</button></li></router-link>
-       <router-link @click="ObrirNav()" to="/mapa"> <li class="hover:bg-red-500 py-2 px-3 text-nowrap rounded-sm"><button >Mapa</button></li></router-link>
-       <router-link @click="ObrirNav()" to="/taula"><li  class="hover:bg-red-500 py-2 px-3 text-nowrap rounded-sm"><button >Taula</button></li></router-link>
-       <router-link @click="ObrirNav()" to="/about"> <li class="hover:bg-red-500 py-2 px-3 text-nowrap rounded-sm"><button>Sobre el Mosaic</button></li></router-link>
-      </ul>
-    </div>
-  </div>
-    </nav>
-    <div class="md:h-20 h-16"></div>
-    <main class="grow ">
-    <router-view/>
-  </main>
-    <footer class="bg-red-600 w-full">
-      <div class="grid md:grid-cols-3 grid-cols-1  justify-between p-5 mx-auto max-w-screen-xl text-white text-center gap-4">
-        <div class="col-span-1"><h3 class="text-lg">Creat per:</h3>Eudald Cardozo Vallhonrat</div>
-        <div class="col-span-1 flex flex-col"><h3 class="text-lg">Informació extreta de:</h3>
-          <a class="font-bold hover:underline" href="https://ca.wikipedia.org/wiki/Llista_de_colles_castelleres" target="blank">ca.wikipedia.org</a>
-          <a class="font-bold hover:underline" href="https://castellscat.cat/ca/les-colles-llistat" target="blank">castellscat.cat</a>
-          <a class="font-bold hover:underline" href="http://www.portalcasteller.cat/v2/colles/" target="blank">portalcasteller.cat</a>
-          <a class="font-bold hover:underline" href="https://sigac.cat/colles" target="blank">sigac.cat</a>
-          
-        </div>
-        <div class="col-span-1"><h3 class="text-lg">Pàgines:</h3> <ul class="flex flex-col  md:items-center text-base gap-1 text-white mt-3 md:mt-0">
-       <router-link to="/"><li class="py-1 px-3 "><button class="hover:underline">Mosaic</button></li></router-link>
-       <router-link to="/liniatemporal"> <li class="py-1 px-3 "><button class="hover:underline">Linia Temporal</button></li></router-link>
-       <router-link to="/mapa"> <li class="py-1 px-3"><button class="hover:underline">Mapa</button></li></router-link>
-       <router-link to="/taula"><li class="py-1 px-3 "><button class="hover:underline">Taula</button></li></router-link>
-       <router-link to="/about"> <li class="py-1 px-3 "><button class="hover:underline">Sobre el Mosaic</button></li></router-link>
-      </ul></div>
-      </div>
-    </footer>
+  <div id="app" class="flex flex-col w-full text-center items-center h-full">
+    <img src="Mosaic-Logo.svg" width="200rem" class="m-12">
+    <h1 class="text-3xl">El Mosaic Casteller s'ha traslladat!</h1>
+    <h2 class="text-2xl">Pots trobar la nova versió a <a class="text-red-600 underline font-bold hover:text-red-500" href="https://mosaiccasteller.netlify.app">mosaiccasteller.netlify.app</a></h2>
   </div>
 </template>
 
-<script>
-import {ref} from '@vue/reactivity'
 
-export default{
-  setup(){
-    let open=ref(false)
 
-    function ObrirNav(){
-      open.value=!open.value
-    }
-
-    return{open,ObrirNav}
-  }
-}
-</script>
 
 <style>
 #app {
